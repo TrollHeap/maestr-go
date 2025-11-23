594605754793ad892715d7e7c88273ff3644b1ac
trollheap <codegrunt@proton.me>
2025-11-23 04:20:29 +0100

    refactor: Update Exercise model structure and move Exercise model to internal package

--- v2-refacto/internal/models/exercise.go
+++ v2-refacto/internal/models/exercise.go
@@ -1,1 +1,22 @@
 package models
+
+import "time"
+
+type Exercise struct {
+	ID             int       `json:"id"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	Domain         string    `json:"domain"`
+	Content        string    `json:"content"`
+	Difficulty     int       `json:"difficulty"`
+	Steps          []string  `json:"steps"`
+	CompletedSteps []int     `json:"completed_steps"`
+	Done           bool      `json:"done"`
+	CreatedAt      time.Time `json:"created_at"`
+}
+
+type ExerciseFilter struct {
+	Domain     string // "Algorithmes", "Go", "" (tous)
+	Status     string // "done", "todo", "" (tous)
+	Difficulty int    // 0 (tous), 1-5
+}
